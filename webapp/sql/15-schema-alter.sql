bf2fea5280f700da38fbc52097225744c093a575
Issei Naruta <naruta@ivry.jp>
2024-12-08 13:08:20 +0900

    Add index for rides

--- webapp/sql/15-schema-alter.sql
+++ webapp/sql/15-schema-alter.sql
@@ -2,6 +2,7 @@ alter table ride_statuses add index idx_ride_id_created_at_asc(ride_id,created_a
 alter table ride_statuses add index idx_ride_id_created_at_desc(ride_id,created_at DESC);
 alter table chair_locations add index idx_chair_id_created_at_asc(chair_id,created_at ASC);
 alter table chair_locations add index idx_chair_id_created_at_desc(chair_id,created_at DESC);
+alter table rides add index idx_chair_id_created_at_asc(chair_id,created_at ASC);
 alter table rides add index idx_chair_id_updated_at_desc(chair_id,updated_at DESC);
 alter table rides add index idx_user_id_created_at_desc(user_id,created_at DESC);
 alter table chairs add index idx_access_token(access_token);
