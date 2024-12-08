34470c82fdf54f32543a17a6600235e46504f3f5
Issei Naruta <naruta@ivry.jp>
2024-12-08 16:38:37 +0900

    Update spec

--- webapp/ruby/spec/isuride_spec.rb
+++ webapp/ruby/spec/isuride_spec.rb
@@ -98,4 +98,56 @@ RSpec.describe 'isuride' do
     end
 
   end
+
+  describe 'chair_handler' do
+    # let(:chair_logged_in_headers) { { 'Cookie' => "chair_session=3013d5ec84e1b230f913a17d71ef27c8d09d777b1cce7a3c1e2ffd4040848411" } }
+    # describe 'POST /api/chair/chairs' do
+    #   it do
+    #     response = request(:post, '/api/chair/chairs', json: {
+    #       name: 'QC-L13-8361',
+    #       model: 'クエストチェア Lite',
+    #       chair_register_token: '7188f2fb45c7d81a6ba30d1572cfff37b3857e66f7b50513bbf89eb2c4bc0ac7'
+    #     })
+    #     expect(response.status).to eq(201)
+    #   end
+    # end
+
+    describe 'POST /api/chair/activity' do
+      it do
+        response = request(:post, '/api/chair/activity', json: {
+          is_active: true
+        }, headers: chair_logged_in_headers)
+        expect(response.status).to eq(204)
+      end
+    end
+
+    describe 'POST /api/chair/coordinate' do
+      it do
+        response = request(:post, '/api/chair/coordinate', json: {
+          latitude: 0,
+          longitude: 0
+        }, headers: chair_logged_in_headers)
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe 'GET /api/chair/notification' do
+      it do
+        response = request(:get, '/api/chair/notification', headers: chair_logged_in_headers)
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe 'POST /api/chair/rides/{ride_id}/status' do
+      it do
+        ride_id = '01JDMP8B58XBZJ5S7A1NW9DSZV'
+        response = request(:post, "/api/chair/rides/#{ride_id}/status", json: {
+          status: 'ENROUTE'
+        }, headers: chair_logged_in_headers)
+        # TODO: 椅子に割り当てられていないライドなのでエラー
+        expect(response.status).to eq(400)
+      end
+    end
+
+  end
 end
