82e5958e53f58214a885f07696f83a8c5f75c1c8
Sorah Fukumori <her@sorah.jp>
2024-12-08 12:24:50 +0900

    log match

--- webapp/ruby/lib/isuride/internal_handler.rb
+++ webapp/ruby/lib/isuride/internal_handler.rb
@@ -14,18 +14,19 @@ module Isuride
       end
 
       10.times do
-        matched = db.query('SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE AND is_busy = FALSE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1').first
-        unless matched
+        matched_id = db.query('SELECT id FROM chairs WHERE is_active = TRUE AND is_busy = FALSE ORDER BY RAND() LIMIT 1').first
+        unless matched_id
           halt 204
         end
 
         db_transaction do |tx|
-          matched = tx.xquery('SELECT * FROM chairs WHERE is_active = TRUE AND is_busy = FALSE AND id = ? LIMIT 1 for update', matched.fetch(:id)).first
-          if matched
-            tx.xquery('UPDATE chairs SET is_busy = TRUE WHERE id = ?', matched.fetch(:id))
-            tx.xquery('UPDATE rides SET chair_id = ? WHERE id = ?', matched.fetch(:id), ride.fetch(:id))
-            halt 204
-          end
+           matched = tx.xquery('SELECT * FROM chairs WHERE is_active = TRUE AND is_busy = FALSE AND id = ? LIMIT 1 for update', matched_id.fetch(:id)).first
+           puts "MATCHING:: chair_id=#{matched.fetch(:id)} ride_id=#{ride.fetch(:id)}"
+           if matched
+             tx.xquery('UPDATE chairs SET is_busy = TRUE WHERE id = ?', matched.fetch(:id))
+             tx.xquery('UPDATE rides SET chair_id = ? WHERE id = ?', matched.fetch(:id), ride.fetch(:id))
+             halt 204
+           end
         end
       end
 
